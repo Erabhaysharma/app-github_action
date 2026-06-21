93f9536e04796d6c07f214858164be08bc34ad64
Abhay kumar <abhaysharma75547@gmail.com>
2026-06-21 17:28:16 +0530

    unit test case added

---
+++ tests/test_opration.py
@@ -0,0 +1,13 @@
+from src.math_opration import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(100,2)==98
+
+    assert sub(5,5)==0
+    assert sub(100,200)==-100
